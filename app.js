3b0fc3e93824626d0812d97665eb9342ac4ee15e
이정우 <ljw3771@kakao.com>
2022-12-07 16:44:23 +0900

    [40기 - 이정우] 로그인 구현 완료

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const { DataSource } = require('typeorm');
 
 dotenv.config();
@@ -32,6 +34,32 @@ appDataSource
 
 app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong' });
+});
+
+app.post('/login', async (req, res, next) => {
+  const { id, password } = req.body;
+  if (!id || !password) {
+    const error = new Error('WRONG INPUT');
+    error.statusCode = 400;
+    throw error;
+  }
+  try {
+    const user = await appDataSource.query(`
+    SELECT * 
+    FROM users u 
+    WHERE u.username = ${id}
+    `);
+    if (user.length === 0) {
+      return res.status(404).json({ message: 'USER_NOT_FOUND' });
+    }
+    const isMatch = await bcrypt.compare(password, user[0].password);
+    if (isMatch) {
+      const token = jwt.sign({ id: user[0].id }, process.env.COOKIE_SECRET);
+      return res.status(200).json({ accessToken: token });
+    }
+  } catch (err) {
+    res.status(err.statusCode || 500).json({ message: err.message });
+  }
 });
 
 const PORT = process.env.PORT;
