e5bf13ac3708da6d41c03ba66b49432f40e56ecc
이정우 <ljw3771@kakao.com>
2022-12-11 00:26:58 +0900

    [40기 - 이정우] 전체상품 query문을 이용한 제품 리스트 필터링 구현 완료

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,9 +1,31 @@
-const { allProducts } = require('../models/productsDao.js');
+const {
+  getProductsByType,
+  allProducts,
+  getProductsBySort,
+  getProductsBySortQuery,
+} = require('../models/productsDao.js');
 
 const getAllProducts = async (req, res) => {
+  const { type, sort } = req.query;
   try {
-    const data = await allProducts();
-    return res.status(200).json(data);
+    if (!type && !sort) {
+      const data = await allProducts();
+      return res.status(200).json(data);
+    }
+    if (!sort) {
+      const result = type.split(',');
+      const data = await getProductsByType(result);
+      return res.status(200).json(data);
+    }
+    if (!type) {
+      const data = await getProductsBySort(sort);
+      return res.status(200).json(data);
+    }
+    if (type && sort) {
+      const result = type.split(',');
+      const data = await getProductsBySortQuery(result, sort);
+      return res.status(200).json(data);
+    }
   } catch (err) {
     return res.status(err.statusCode || 500).json(err.message);
   }
--- models/productsDao.js
+++ models/productsDao.js
@@ -45,6 +45,259 @@ const allProducts = async () => {
   }
 };
 
+const getProductsByType = async (type) => {
+  try {
+    if (type.length === 1) {
+      const data = await appDataSource.query(`
+      SELECT 
+      p.name,
+      p.price,
+      i.main_image,
+      i.sub_image
+      FROM 
+      products p
+      INNER JOIN 
+      images i 
+      ON 
+      p.image_id = i.id
+      INNER JOIN 
+      product_types t
+      ON 
+      p.product_type_id = t.id
+      WHERE 
+      t.name = '${type[0]}'
+      `);
+      return data;
+    }
+    if (type.length === 2) {
+      const data = await appDataSource.query(`
+          SELECT 
+          p.name,
+          p.price,
+          i.main_image,
+          i.sub_image
+          FROM 
+          products p
+          INNER JOIN 
+          images i 
+          ON 
+          p.image_id = i.id
+          INNER JOIN 
+          product_types t
+          ON 
+          p.product_type_id = t.id
+          WHERE 
+          t.name = '${type[0]}' OR t.name = '${type[1]}'
+          `);
+      return data;
+    }
+    if (type.length === 3) {
+      const data = await appDataSource.query(`
+            SELECT 
+            p.name,
+            p.price,
+            i.main_image,
+            i.sub_image
+            FROM 
+            products p
+            INNER JOIN 
+            images i 
+            ON 
+            p.image_id = i.id
+            INNER JOIN 
+            product_types t
+            ON 
+            p.product_type_id = t.id
+            WHERE 
+            t.name = '${type[0]}' OR t.name = '${type[1]}' OR t.name = '${type[2]}'
+            `);
+      return data;
+    }
+    if (type.length === 4) {
+      const data = await appDataSource.query(`
+            SELECT 
+            p.name,
+            p.price,
+            i.main_image,
+            i.sub_image
+            FROM 
+            products p
+            INNER JOIN 
+            images i 
+            ON 
+            p.image_id = i.id
+            INNER JOIN 
+            product_types t
+            ON 
+            p.product_type_id = t.id
+            WHERE 
+            t.name = '${type[0]}' OR t.name = '${type[1]}' OR t.name = '${type[2]} 'OR t.name = '${type[3]}'
+            `);
+      return data;
+    }
+  } catch (err) {
+    console.log(err);
+    const error = new Error('DATABASE_ERROR');
+    error.statusCode = 500;
+    throw error;
+  }
+};
+
+const getProductsBySort = async (sort) => {
+  let order;
+  if (sort === 'new-arrival') {
+    order = 'p.created_at DESC';
+  }
+  if (sort === 'price-desc') {
+    order = 'p.price DESC';
+  }
+  if (sort === 'price-asc') {
+    order = 'p.price';
+  }
+  try {
+    const data = await appDataSource.query(`
+        SELECT 
+        p.name,
+        p.price,
+        i.main_image,
+        i.sub_image
+        FROM 
+        products p
+        INNER JOIN 
+        images i
+        ON 
+        p.image_id = i.id
+        ORDER BY 
+        ${order}
+        `);
+    return data;
+  } catch (err) {
+    console.log(err);
+    const error = new Error('DATABASE_ERROR');
+    error.statusCode = 500;
+    throw error;
+  }
+};
+
+const getProductsBySortQuery = async (type, sort) => {
+  let order;
+  if (sort === 'new-arrival') {
+    order = 'p.created_at DESC';
+  }
+  if (sort === 'price-desc') {
+    order = 'p.price DESC';
+  }
+  if (sort === 'price-asc') {
+    order = 'p.price';
+  }
+  try {
+    if (type.length === 1) {
+      const data = await appDataSource.query(`
+      SELECT 
+      p.name,
+      p.price,
+      i.main_image,
+      i.sub_image
+      FROM 
+      products p
+      INNER JOIN 
+      images i 
+      ON 
+      p.image_id = i.id
+      INNER JOIN 
+      product_types t
+      ON 
+      p.product_type_id = t.id
+      WHERE 
+      t.name = '${type[0]}'
+      ORDER BY
+      ${order}
+      `);
+      return data;
+    }
+    if (type.length === 2) {
+      const data = await appDataSource.query(`
+          SELECT 
+          p.name,
+          p.price,
+          i.main_image,
+          i.sub_image
+          FROM 
+          products p
+          INNER JOIN 
+          images i 
+          ON 
+          p.image_id = i.id
+          INNER JOIN 
+          product_types t
+          ON 
+          p.product_type_id = t.id
+          WHERE 
+          t.name = '${type[0]}' OR t.name = '${type[1]}'
+          ORDER BY
+          ${order}
+          `);
+      return data;
+    }
+    if (type.length === 3) {
+      const data = await appDataSource.query(`
+            SELECT 
+            p.name,
+            p.price,
+            i.main_image,
+            i.sub_image
+            FROM 
+            products p
+            INNER JOIN 
+            images i 
+            ON 
+            p.image_id = i.id
+            INNER JOIN 
+            product_types t
+            ON 
+            p.product_type_id = t.id
+            WHERE 
+            t.name = '${type[0]}' OR t.name = '${type[1]}' OR t.name = '${type[2]}'
+            ORDER BY
+            ${order}
+            `);
+      return data;
+    }
+    if (type.length === 4) {
+      const data = await appDataSource.query(`
+            SELECT 
+            p.name,
+            p.price,
+            i.main_image,
+            i.sub_image
+            FROM 
+            products p
+            INNER JOIN 
+            images i 
+            ON 
+            p.image_id = i.id
+            INNER JOIN 
+            product_types t
+            ON 
+            p.product_type_id = t.id
+            WHERE 
+            t.name = '${type[0]}' OR t.name = '${type[1]}' OR t.name = '${type[2]} 'OR t.name = '${type[3]}'
+            ORDER BY 
+            ${order}
+            `);
+      return data;
+    }
+  } catch (err) {
+    console.log(err);
+    const error = new Error('DATABASE_ERROR');
+    error.statusCode = 500;
+    throw error;
+  }
+};
+
 module.exports = {
   allProducts,
+  getProductsByType,
+  getProductsBySort,
+  getProductsBySortQuery,
 };
